a7b6a0ec94634cad8f4375c14d8f3548511e869f
Max Clayton Clowes <contact@mcclowes.com>
2023-07-26 16:08:05 +0100

    Fix components folder depth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { FormEvent, useState } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-import { CodatLink } from "./components/CodatLink";
+import { CodatLink } from "../components/CodatLink";
 
 import styles from "./styles.module.scss";
 
